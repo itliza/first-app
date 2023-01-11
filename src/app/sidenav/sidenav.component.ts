8d06203db4621c32c136b4f407a749dc0066f3f3
Liza Romaniuk <elizaveta.romaniuk@gmail.com>
2023-01-11 13:39:06 +0200

    +

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,83 +1,70 @@
-// import {Component} from "@angular/core";
+import {Component} from "@angular/core";
 
-
-// @Component({
-//   selector:'app-sidenav',
-//   templateUrl: './sidenav.component.html',
-//   styleUrls: ['./sidenav.component.css']
-// })
-// export class SidenavComponent { }
-
-import {FlatTreeControl} from '@angular/cdk/tree';
-import {Component} from '@angular/core';
-import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
-
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Settings',
-    children: [{name: 'Users'}, {name: 'Vehicles'}, {name: 'Sources'}],
-  },
-  // {
-  //   name: 'Vegetables',
-  //   children: [
-  //     {
-  //       name: 'Green',
-  //       children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-  //     },
-  //     {
-  //       name: 'Orange',
-  //       children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-  //     },
-  //   ],
-  // },
-];
-
-
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-}
 
 @Component({
-  selector: 'app-sidenav',
+  selector:'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
-  private _transformer = (node: FoodNode, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-    };
-  };
+export class SidenavComponent { }
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level,
-    node => node.expandable,
-  );
+// import {FlatTreeControl} from '@angular/cdk/tree';
+// import {Component} from '@angular/core';
+// import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
-  );
+// interface FoodNode {
+//   name: string;
+//   children?: FoodNode[];
+// }
 
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+// const TREE_DATA: FoodNode[] = [
+//   {
+//     name: 'Settings',
+//     children: [{name: 'Users'}, {name: 'Vehicles'}, {name: 'Sources'}],
+//   },
+// ];
 
-  constructor() {
-    this.dataSource.data = TREE_DATA;
-  }
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-}
+// interface ExampleFlatNode {
+//   expandable: boolean;
+//   name: string;
+//   level: number;
+// }
+
+// @Component({
+//   selector: 'app-sidenav',
+//   templateUrl: './sidenav.component.html',
+//   styleUrls: ['./sidenav.component.css']
+// })
+// export class SidenavComponent {
+//   private _transformer = (node: FoodNode, level: number) => {
+//     return {
+//       expandable: !!node.children && node.children.length > 0,
+//       name: node.name,
+//       level: level,
+//     };
+//   };
+
+//   treeControl = new FlatTreeControl<ExampleFlatNode>(
+//     node => node.level,
+//     node => node.expandable,
+//   );
+
+//   treeFlattener = new MatTreeFlattener(
+//     this._transformer,
+//     node => node.level,
+//     node => node.expandable,
+//     node => node.children,
+//   );
+
+//   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+//   constructor() {
+//     this.dataSource.data = TREE_DATA;
+//   }
+
+//   hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+// }
 
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
