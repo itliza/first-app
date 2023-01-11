2a9b8583e9fa71629b74faf753e42c8b27851d90
ninasavkina <64922151+ninasavkina@users.noreply.github.com>
2023-01-11 16:01:28 +0200

    fix filters

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,34 +1,35 @@
-// import {Component, ViewChild} from '@angular/core';
-// import {MatAccordion} from '@angular/material/expansion';
-//
-// /**
-//  * @title Accordion with expand/collapse all toggles
-//  */
-// @Component({
-//   selector: 'app-filters',
-//   templateUrl: './filters.component.html',
-//   styleUrls: ['./filters.component.css'],
-// })
-// export class FiltersComponent {
-//   @ViewChild(MatAccordion) accordion: MatAccordion;
-// }
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatAccordion} from '@angular/material/expansion';
 
+/**
+ * @title Accordion with expand/collapse all toggles
+ */
 @Component({
   selector: 'app-filters',
-  styleUrls: ['./filters.component.css'],
   templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent {
-  bold = false;
-  italic = false;
-
-  sizes = ['Small', 'Normal', 'Large'];
-  selectedSize: string | undefined = 'Normal';
-
-  reset() {
-    this.bold = false;
-    this.italic = false;
-    this.selectedSize = 'Normal';
-  }
+  @ViewChild(MatAccordion) accordion: MatAccordion;
 }
+// import {Component} from '@angular/core';
+//
+// @Component({
+//   selector: 'app-filters',
+//   styleUrls: ['./filters.component.css'],
+//   templateUrl: './filters.component.html',
+// })
+// export class FiltersComponent {
+//   bold = false;
+//   italic = false;
+//
+//   sizes = ['Small', 'Normal', 'Large'];
+//   selectedSize: string | undefined = 'Normal';
+//
+//
+//   reset() {
+//     this.bold = false;
+//     this.italic = false;
+//     this.selectedSize = 'Normal';
+//   }
+// }
